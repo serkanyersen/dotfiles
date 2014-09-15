4fad297e33400869589054d682c708fddb115a93
Serkan Yersen <serkanyersen@gmail.com>
2014-09-15 12:47:13 -0700

    add colors and shit

--- to.sh
+++ to.sh
@@ -1,11 +1,53 @@
 #!/bin/bash
+#####################################################################################
+# Installation
+# ------------
+#
+# 1) Copy script to your home folder
+# -$ cd ~
+# -$ wget https://raw.githubusercontent.com/serkanyersen/dotfiles/master/to.sh
+#
+# 2) Open file and change defaults for your needs
+#
+# 3) Give executable permissions
+# -$ chmod +x to.sh
+#
+# 4) Link it to your bin folder (as `to` not `to.sh`)
+#  -) For mac os x
+#     -$ ln -s ~/to.sh /usr/local/bin/to
+#  -) For linux
+#     -$ ln -s ~/to.sh /usr/bin/to
+#
+# That's it, Enjoy :)
+#####################################################################################
 
-usage() { echo -e "Usage: $0\n\t[-h <hostname>] Host Name\n\t[-u <user>] Username\n\t[-n] Don't use tmux\n\t[-s] Use SSH instead of MOSH" 1>&2; exit 0; }
+# Colors
+GRAY="\033[1;30m"
+GREEN="\033[0;32m"
+CYAN="\033[0;36m"
+LIGHT_CYAN="\033[1;36m"
+NO_COLOR="\033[0m"
 
+# Defaults
 HOST='addv4'
 USER='serkan'
+
+# Configs
 TMUX='-- tmux attach -d'
 CMD='mosh'
+
+# Display usage
+usage() {
+    echo -e "$GREEN Usage$NO_COLOR: Calling without arguments will connect using defaults
+$GREEN Defaults$NO_COLOR: $CMD $USER@$HOST $TMUX
+$GREEN Parameters$NO_COLOR:
+    [-h <hostname>]$GRAY Change host name$NO_COLOR
+    [-u <user>]$GRAY Change username$NO_COLOR
+    [-n]$GRAY Don't attach tmux$NO_COLOR
+    [-s]$GRAY Use SSH instead of MOSH$NO_COLOR
+    [--h]$GRAY Display this message$NO_COLOR" 1>&2;
+    exit 0;
+}
 
 while getopts ":h:u:ns" o; do
     case "${o}" in
@@ -33,4 +75,5 @@ done
 
 echo Connecting to $HOST
 $CMD $USER@$HOST $TMUX
+
 exit 1
