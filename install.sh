77d1c0ee88e51f15d165124f90e8fb13f1616c42
Serkan Yersen <serkanyersen@gmail.com>
2015-02-09 00:02:06 -0800

    Add to.sh install

--- install.sh
+++ install.sh
@@ -6,37 +6,41 @@ then
   exit 0
 fi
 
-echo "Backing up existing files."
+echo "- Backing up existing files."
 mkdir ~/.dotfiles-backup
 mv ~/.vim* ~/.dotfiles-backup
 mv ~/.zshrc ~/.dotfiles-backup
 mv ~/.gitconfig ~/.dotfiles-backup
 mv ~/.ssh/config ~/.dotfiles-backup/.ssh-config
 mv ~/.tmux.conf ~/.dotfiles-backup
-echo "backup done: ~/.dotfiles-backup"
+echo "-- backup done: ~/.dotfiles-backup"
 
 if [[ $SHELL != *"/zsh"* ]]
 then
-  echo "Setting zsh as default for current user."
+  echo "- Setting zsh as default for current user."
   sudo chsh -s $(which zsh) $(whoami)
 fi
 
 if [[ -d $HOME/.oh-my-zsh ]]
 then
-  echo "oh-my-zsh is already installed."
+  echo "- oh-my-zsh is already installed."
 else
-  echo "Installing oh-my-zsh."
+  echo "- Installing oh-my-zsh."
   curl -L http://install.ohmyz.sh | sh
 fi
 
-echo "Copying new dotfiles"
+echo "- Copying new dotfiles"
 ln -s $HOME/dotfiles/config/.vimrc $HOME
 ln -s $HOME/dotfiles/config/.zshrc $HOME
 ln -s $HOME/dotfiles/config/.gitconfig $HOME
 ln -s $HOME/dotfiles/config/.sshconfig $HOME/.ssh/config
 ln -s $HOME/dotfiles/config/.tmux.conf $HOME
 
-echo "Files copied, enabling."
+echo "- Installing 'to' script."
+ln -s $HOME/dotfiles/scripts/to.sh $HOME/bin/to
+chmod +x $HOME/bin/to
+
+echo "- Files copied, enabling."
 source $HOME/.zshrc
 
 # put this file here so we know install was done before
