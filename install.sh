47ce71554f4d6be24264baed25a7fa8d5b0108a0
Serkan Yersen <serkanyersen@gmail.com>
2018-10-24 02:37:25 -0700

    final updates

--- install.sh
+++ install.sh
@@ -95,10 +95,10 @@ elif [[ "$OSTYPE" == "linux"* ]]; then
   test -r ~/.bash_profile && echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.bash_profile
   echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.profile
   echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >> ~/.exportsrc
-  echo "LC_ALL=en_US.UTF-8" >> /etc/environment
-  echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-  echo "LANG=en_US.UTF-8" > /etc/locale.conf
-  locale-gen en_US.UTF-8
+  # sudo echo "LC_ALL=en_US.UTF-8" >> /etc/environment
+  # sudo echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+  # sudo echo "LANG=en_US.UTF-8" > /etc/locale.conf
+  # sudo locale-gen en_US.UTF-8
   mv ~/.gitconfig.tmp ~/.gitconfig
 fi
 
